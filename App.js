c2566d8f7dda1a894f08c630303ae0b1dce769d7
mihalichpalich <pavlovtfspb@gmail.com>
2020-03-11 22:52:13 +0300

    начал

--- App.js
+++ App.js
@@ -1,19 +1,51 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components/native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Container>
+        <Group>
+            <GroupTitle>11 сентября</GroupTitle>
+            <GroupItem>
+                <Avatar/>
+                <FullName>Анжела Матиева</FullName>
+            </GroupItem>
+        </Group>
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const FullName = styled.Text`
+  font-weight: 600;
+  font-size: 16px;  
+`;
+
+const Avatar = styled.View`
+  margin-right: 15px;
+  width: 40px;
+  height: 40px;
+  border-radius: 50px;  
+  background-color: yellow;
+`;
+
+const GroupItem = styled.View`
+  flex-direction: row;
+  padding: 20px 0;
+`;
+
+const Group = styled.View`
+  padding: 0 20px;
+`;
+
+const GroupTitle = styled.Text`
+  font-weight: 800;
+  font-size: 22px;
+  color: #000;
+`;
+
+const Container = styled.View`
+  flex: 1;
+  margin-top: 50px;  
+`;
+
