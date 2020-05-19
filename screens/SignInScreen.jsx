a57e14da0381c34de47ce32cda298f208d588949
mihalichpalich <pavlovtfspb@gmail.com>
2020-05-20 02:30:42 +0300

    продолжаю переделывать регистрацию

--- screens/SignInScreen.jsx
+++ screens/SignInScreen.jsx
@@ -1,10 +1,9 @@
 import React, {useState} from "react";
 import styled from 'styled-components/native';
-import {Text, View} from 'react-native';
+import {Text, View, Button} from 'react-native';
 import { Item, Input, Label } from 'native-base';
 import * as Google from 'expo-google-app-auth';
 
-import {Button, Container} from '../components';
 import {usersApi, programsApi, lessonsApi} from "../utils/api";
 
 const SignInScreen = ({navigation}) => {
@@ -13,7 +12,7 @@ const SignInScreen = ({navigation}) => {
         googleID: ''
     });
 
-    const signIn = async function signInWithGoogleAsync() {
+    const onSubmit = async function signInWithGoogleAsync() {
         try {
             const result = await Google.logInAsync({
                 androidClientId: "588758140087-i1d4o42r9i56cbcdift9679trblfrtv8.apps.googleusercontent.com",
@@ -25,6 +24,11 @@ const SignInScreen = ({navigation}) => {
                     email: result.user.email,
                     googleID: result.user.id
                 });
+                usersApi.signIn(values).then(() => {
+                    navigation.navigate('Home');
+                }).catch(e => {
+                    console.log(e);
+                })
             } else {
                 console.log('cancelled');
             }
@@ -33,105 +37,24 @@ const SignInScreen = ({navigation}) => {
         }
     };
 
-    const onSubmit = () => {
-        usersApi.login(values).then((res) => {
-            if (res.status === 404) {
-                alert('Введен неправильный номер телефона или пароль');
-            } else if (res.status === 200) {
-                usersApi
-                    .getByPhone(values.phone)
-                    .then(({data}) => {
-                        const userId = data.data.user._id;
-
-                        programsApi.get(userId)
-                            .then(({data}) => {
-                                if (data.data.length == 0) {
-                                    navigation.navigate('Program', {user: userId});
-                                } else {
-                                    lessonsApi.get(userId)
-                                        .then(({data}) => {
-                                            if (data.data.length == 0) {
-                                                navigation.navigate('Students', {user: userId});
-                                            } else {
-                                                navigation.navigate('Home', {user: userId});
-                                            }
-                                        })
-                                        .catch(e => {
-                                            console.log(e);
-                                        });
-                                }
-                            })
-                            .catch(e => {
-                                console.log(e);
-                            });
-                    })
-                    .catch(e => {
-                        console.log(e);
-                    });
-            }
-        })
-            .catch(
-                e => {
-                    console.log(e);
-                    if (e.response.data && e.response.data.message) {
-                        e.response.data.message.forEach(err => {
-                            console.log(err);
-
-                            if (err.value === "") {
-                                const fieldName = err.param;
-                                alert(`Ошибка! Поле "${fieldsName[fieldName]}" пустое.`);
-                            }
-                        });
-                    }
-                }
-            );
-    };
-
     return (
-        <Container>
-            <Item style={{marginLeft: 0}} floatingLabel>
-                <Label>Телефон</Label>
-                <Input
-                    onChange={handleInputChange.bind(this, 'phone')}
-                    value={values.phone}
-                    autoFocus
-                    style={{marginTop: 12}}
-                />
-            </Item>
-
-            <Item style={{marginTop: 20, marginLeft: 0}} floatingLabel>
-                <Label>Пароль</Label>
-                <Input
-                    onChange={handleInputChange.bind(this, 'password')}
-                    value={values.password}
-                    secureTextEntry={true}
-                    style={{marginTop: 12}}
-                />
-            </Item>
-
-            <ButtonView>
-                <Button onPress={onSubmit} color="#87CC6F">
-                    <Text>Вход</Text>
-                </Button>
-            </ButtonView>
-
-            <View style={{flex: 3}}>
-                <RegisterLink onPress={navigation.navigate.bind(this, 'Registration')}>Зарегистрироваться</RegisterLink>
-            </View>
-        </Container>
+        <SignInContainer>
+            <Button title="Войти с помощью Google" onPress={onSubmit}/>
+        </SignInContainer>
     )
 };
 
-const ButtonView = styled.View`
-  flex: 1;
-  margin-top: 30px;
+const SignInContainer = styled.View`
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+    background-color: #fff;
 `;
 
-const RegisterLink = styled.Text`
-  text-align: center;
-  font-size: 16px;
-  color: dodgerblue;
+const SignInHeader = styled.Text`
+    font-size: 25px;
 `;
+
 
 SignInScreen.navigationOptions = {
     title: "Вход",
--- utils/api/users.js
+++ utils/api/users.js
@@ -1,9 +1,5 @@
 import axios from '../../core/axios';
 
 export default {
-  registration: values => axios.post('/user/registration', values),
-  getByPhone: phone => axios.get('/user/getbyphone/' + phone),
-  getConfirmCode: id => axios.get('/user/getconfirmcode/' + id),
-  confirm: id => axios.get('/user/confirm/' + id),
-  login: values => axios.post('/user/login', values, { validateStatus: (status) => status === 404 || 200})
+  signIn: values => axios.post('/user/sigh_in', values)
 };
